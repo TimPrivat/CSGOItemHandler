01b567646cafa67ec20ddc3c889109a5e23cec2e
TimPrivat <timleon2701@gmail.com>
2023-10-12 16:03:19 +0200

    Add SpringProject

---
+++ src/main/java/com/Home/Tim/CSGOItemHandler/CsgoItemHandlerApplication.java
@@ -0,0 +1,13 @@
+package com.Home.Tim.CSGOItemHandler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsgoItemHandlerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CsgoItemHandlerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Home/Tim/CSGOItemHandler/CsgoItemHandlerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Home.Tim.CSGOItemHandler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsgoItemHandlerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
